908d65bf9dea5e3523e9334cec3f896f4aca6d9f
SEJUNHONG <57587248+SEJUNHONG@users.noreply.github.com>
2019-11-10 19:05:17 +0900

    DSPE-Python
    Data Type

--- Assignment1.py
+++ Assignment1.py
@@ -1,13 +1,31 @@
-import torch
-import socket
-import platform
+import random
+import time
 
-ip_address = socket.gethostbyname(socket.gethostname())
+w=["cat", "dog", "fox", "monkey", "mouse", "panda", "frog", "snake", "wolf"]
 
-if torch.cuda.is_available():
-    print("cuda is available")
-else:
-    print("cuda is not available")
+n=1
+print("[타자 게임] 준비되면 엔터!")
 
-    print(ip_address)
-    print(platform.uname(), "\n")
+input()
+
+start=time.time()
+
+q=random.choice(w)
+
+while n<=5:
+    print("*문제", n)
+    print(q)
+    x=input()
+    if q==x:
+        print("통과!")
+        n=n+1
+        q=random.choice(w)
+    else:
+        print("오타! 다시 도전!")
+
+end=time.time()
+
+et=end-start
+
+et=format(et, ".2f")
+print("타자 시간 : ", et, "초")
